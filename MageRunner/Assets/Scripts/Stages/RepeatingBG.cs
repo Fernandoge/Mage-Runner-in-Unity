64eed358c8499d0aa5466c6738fc40556f54cfa2
Fernandoge <fernando.gp101@gmail.com>
2020-04-16 22:38:23 -0400

    Added logic to repeat the background and make a parallax effect

---
+++ MageRunner/Assets/Scripts/Stages/RepeatingBG.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingBG : MonoBehaviour
+{
+    public float speed;
+    public float startX;
+    public float endX;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+        
+        if (transform.position.x <= endX)
+        {
+            transform.position = new Vector2(startX, transform.position.y);
+        }
+    }
+}
