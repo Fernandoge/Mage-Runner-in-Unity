1cad87c1f20f37c0dbc8902f0fca794433e160fa
Fernandoge <fernando.gp101@gmail.com>
2020-05-17 22:52:12 -0400

    Added debug button to enable and disable level movement

---
+++ MageRunner/Assets/Scripts/System/DebugController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugController : MonoBehaviour
+{
+    public LevelController level;
+
+    public void EnableLevel()
+    {
+        level.enabled = level.isActiveAndEnabled ? false : true;
+    }
+}
