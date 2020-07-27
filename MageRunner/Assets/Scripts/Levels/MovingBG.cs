b62a2252be4c6aa18a0a079f4ba9354c1b090424
Fernandoge <fernando.gp101@gmail.com>
2020-07-26 22:49:38 -0400

    Updated background images size to reduce overdraw

---
+++ MageRunner/Assets/Scripts/Levels/MovingBG.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class MovingBG : MonoBehaviour
+{
+    public float speed;
+    
+    private void Update()
+    {
+        if (GameManager.Instance.player.isMoving == false)
+            return;
+
+        float totalSpeed = GameManager.Instance.level.movingSpeed * speed;
+        transform.Translate(Vector2.left * totalSpeed * Time.deltaTime);
+    }
+}
